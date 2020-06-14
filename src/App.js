d5aeaef55d9220277b4bcc7d668f111d3d7770b9
yambo1992 <1810962886@qq.com>
2020-06-14 23:06:14 +0800

    lazy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React, { useEffect, useReducer, useState } from 'react'
 import appReducer from './reducers'
-import UserBar from './user/UserBar'
-import CreatePost from './post/CreatePost'
-import PostList from './post/PostList'
-import Header from './Header'
-import ChangeTheme from './ChangeTheme'
+import HeaderBar from './pages/HeaderBar'
+import HomePage from './pages/HomePage'
 import { ThemeContext, StateContext } from './contexts'
-import { useResource } from 'react-request-hook'
+
 
 
 export default function App () {
@@ -17,21 +14,7 @@ export default function App () {
   const [ state, dispatch ] = useReducer(appReducer, { user: '', posts: [], error: '' })
   const { user, error } = state;
 
-  const [ posts, getPosts ] = useResource(() => ({
-    url: '/posts',
-    method: 'get'
-  }))
-
-  useEffect(getPosts, [])
-
-  useEffect(() => {
-    if(posts && posts.error) {
-      dispatch({ type: 'POSTS_ERROR' })
-    }
-    if(posts && posts.data) {
-      dispatch({ type: 'FETCH_POSTS', posts: posts.data.reverse() })
-    }
-  }, [posts])
+ 
 
   useEffect(() => {
     if(user) {
@@ -45,18 +28,9 @@ export default function App () {
     <StateContext.Provider value={{ state, dispatch }}>
       <ThemeContext.Provider value={theme}>
         <div style={{ padding: 8 }}>
-          <Header text="React Hooks Blog" />
-          <ChangeTheme theme={theme} setTheme={setTheme} />
-          <br/>
-          <React.Suspense fallback={"Loading..."}>
-            <UserBar />
-          </React.Suspense>
-          <br />
-          { user && <CreatePost />}
-          <br />
+         <HeaderBar setTheme={ setTheme } />
           <hr />
-          {error && <b>{error}</b>}
-          <PostList/>
+          <HomePage />
         </div>
       </ThemeContext.Provider>
     </StateContext.Provider>
